cee1799ff973e70c65f018f4244b5726745f690d
Dannee Kim <dannee.kim.dev@gmail.com>
2022-02-01 18:21:12 -0500

    convert navbar to stateless function component

--- react-counter-app/src/components/navbar.tsx
+++ react-counter-app/src/components/navbar.tsx
@@ -1,18 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class NavBar extends Component<any, any> {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand mb-0 h1">
-            Navbar{" "}
-            <span className="badge rounded-pill bg-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+const Navbar = (props: any) => {
+  const { totalCounters } = props;
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand mb-0 h1">
+          Navbar{" "}
+          <span className="badge rounded-pill bg-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+// export default class NavBar extends Component<any, any> {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <div className="container-fluid">
+//           <a className="navbar-brand mb-0 h1">
+//             Navbar{" "}
+//             <span className="badge rounded-pill bg-secondary">
+//               {this.props.totalCounters}
+//             </span>
+//           </a>
+//         </div>
+//       </nav>
+//     );
+//   }
+// };
