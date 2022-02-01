d666d8aa2b255fa45a2754395dd908a2916801e0
Dannee Kim <dannee.kim.dev@gmail.com>
2022-02-01 18:23:50 -0500

    obj destructuring of props

--- react-counter-app/src/components/counters.tsx
+++ react-counter-app/src/components/counters.tsx
@@ -3,22 +3,42 @@ import Counter from "./counter";
 
 export default class Counters extends Component<any, any> {
   render() {
+    //obj destructuring
+    const { onReset, onIncrement, onDelete, counters } = this.props;
+
     return (
       <div>
-        <button onClick={this.props.onReset} className="btn btn-primary m-2">
+        <button onClick={onReset} className="btn btn-primary m-2">
           Reset
         </button>
-        {this.props.counters.map((counter: any) => (
+        {counters.map((counter: any) => (
           <Counter
             key={counter.id}
             counter={counter}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
           >
             <h4>Counter #{counter.id}</h4>
           </Counter>
         ))}
       </div>
     );
+    // return (
+    //   <div>
+    //     <button onClick={this.props.onReset} className="btn btn-primary m-2">
+    //       Reset
+    //     </button>
+    //     {this.props.counters.map((counter: any) => (
+    //       <Counter
+    //         key={counter.id}
+    //         counter={counter}
+    //         onIncrement={this.props.onIncrement}
+    //         onDelete={this.props.onDelete}
+    //       >
+    //         <h4>Counter #{counter.id}</h4>
+    //       </Counter>
+    //     ))}
+    //   </div>
+    // );
   }
 }
