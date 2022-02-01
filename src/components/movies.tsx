1e00de40e34781e065b6fda1e296c94442104868
Dannee Kim <dannee.kim.dev@gmail.com>
2022-02-01 15:53:12 -0500

    rendering movies in table and button from fakeMovieService

---
+++ src/components/movies.tsx
@@ -0,0 +1,46 @@
+import { AnyCnameRecord } from "dns";
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+export default class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie: any) => {
+    console.log(movie);
+  };
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-sm btn-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
