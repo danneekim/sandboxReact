0870c61e186efd6c0736c17e9f4f1a87b27a0246
Dannee Kim <dannee.kim.dev@gmail.com>
2022-02-01 18:03:47 -0500

    create navbar component

---
+++ react-counter-app/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">Navbar</span>
+        </div>
+      </nav>
+    );
+  }
+}
