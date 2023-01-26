1c6d569aa7bde7a005f3f14f311adad65bbd91ee
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-26 16:46:19 -0500

    create header component

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// CSS in JS
+// const headingStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
