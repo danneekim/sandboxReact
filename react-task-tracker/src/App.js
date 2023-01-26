d2d6a38bbad72f755b86e07adb519293cf967384
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-26 17:01:58 -0500

    create Tasks component w. dummy data

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
 
 function App() {
   return (
     <div className="container">
       <Header title={"Task Tracker"} />
+      <Tasks />
     </div>
   );
 }
