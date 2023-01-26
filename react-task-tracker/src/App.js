83852db765b0502e922a7ba215783ebbd377a21e
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-26 17:06:11 -0500

    move static tasks to App state

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { Tasks } from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header title={"Task Tracker"} />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,19 +1,4 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true,
-  },
-];
-
-export const Tasks = () => {
+export const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
