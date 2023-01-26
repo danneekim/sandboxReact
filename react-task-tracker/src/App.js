b4aa9b47316479663e5fcaafa97297df9ad42aca
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-26 17:25:45 -0500

    display text when no deletable tasks

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -28,7 +28,11 @@ function App() {
   return (
     <div className="container">
       <Header title={"Task Tracker"} />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
 }
