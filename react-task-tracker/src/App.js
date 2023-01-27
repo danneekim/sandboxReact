a3d60b9dacdfee68ae13bfbe38304cb6c52bfd09
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-27 14:30:21 -0500

    update addTask to add Task to server

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -25,10 +25,22 @@ function App() {
   };
 
   // Add Task
-  const addTask = (task) => {
-    const id = tasks.slice(-1)[0].id + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
+
+    // const id = tasks.slice(-1)[0].id + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
   };
 
   // Delete Task
