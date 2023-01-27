6e44cfa2627c0397d21c647ad97182a7d13d76ca
Dannee Kim <dannee.kim.dev@gmail.com>
2023-01-27 14:18:10 -0500

    install json-server dependencyand move tasks to db.json

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -6,20 +6,7 @@ import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   // Add Task
   const addTask = (task) => {
